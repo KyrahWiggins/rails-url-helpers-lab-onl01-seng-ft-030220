059da0297b1a45f79f1b7c10e30f9fd70c64edd2
Kyrah Wiggins <kyrah.wiggins@gmail.com>
2020-07-18 07:14:45 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,16 +1,16 @@
 def index
 
-end 
+end
 def show
     # @student = Student.find(params[:id])
   end
 
-  def activate 
+  def activate
     @student = Student.find(params[:id])
     @student.active = !@student.active
     @student.save
     redirect_to student_path(@student)
-  end	
+  end
 
 
   private	 
