6c9bdbcfcc25aee3e5169ae1c81b9b38b233ad18
Kyrah Wiggins <kyrah.wiggins@gmail.com>
2020-07-18 07:27:13 +0000

    Automatically backed up by Learn

--- spec/features/student_spec.rb
+++ spec/features/student_spec.rb
@@ -1,5 +1,5 @@
 expect(page).to have_css("h1", text: "Targaryen")
-end
+
 	 # it 'renders the active status if the user is inactive' do
     #   visit student_path(@student)
   #   expect(page).to have_content("This student is currently inactive.")
