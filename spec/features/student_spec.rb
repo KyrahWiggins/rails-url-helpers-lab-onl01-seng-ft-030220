ce8072687138daeceb861b0073f09186f238b587
Kyrah Wiggins <kyrah.wiggins@gmail.com>
2020-07-18 07:30:50 +0000

    Automatically backed up by Learn

--- spec/features/student_spec.rb
+++ spec/features/student_spec.rb
@@ -1,5 +1,5 @@
 expect(page).to have_css("h1", text: "Targaryen")
-
+end
 	 # it 'renders the active status if the user is inactive' do
     #   visit student_path(@student)
   #   expect(page).to have_content("This student is currently inactive.")
@@ -12,5 +12,5 @@ expect(page).to have_css("h1", text: "Targaryen")
    #   visit student_path(@student)
  #   expect(page).to have_content("This student is currently active.")
   # end
-describe 'Activate page' do
-end
+  it "Should redirect to the student show page" do
+      visit activate_student_path(@student)	    visit activate_student_path(@student)
